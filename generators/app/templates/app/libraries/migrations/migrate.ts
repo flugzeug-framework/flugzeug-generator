7eaa28ce9c882dfbaa96a626b62945208d814103
Angel Caamal <caamalcanulangel@gmail.com>
2022-01-03 13:11:10 -0600

    Add migrations files.

--- generators/app/templates/app/libraries/migrations/migrate.ts
+++ generators/app/templates/app/libraries/migrations/migrate.ts
@@ -1,30 +1,6 @@
-require("dotenv").config();
-
-import { log } from "@/libraries/Log";
+import { migrate } from "flugzeug";
 import { db } from "@/db";
-import path from "path";
-import Umzug from "umzug";
 
-const umzug = new Umzug({
-  migrations: {
-    path: path.join(__dirname, "./../../migrations"),
-    params: [db.getQueryInterface()],
-  },
-  storage: "sequelize",
-  storageOptions: {
-    sequelize: db,
-  },
-});
-
-async function main() {
-  try {
-    await umzug.up();
-    log.info("MIGRATIONS DONE");
-  } catch (err) {
-    log.error(err);
-    process.exit();
-  }
-  process.exit();
-}
-
-main();
+(async function() {
+  await migrate(db);
+})();
